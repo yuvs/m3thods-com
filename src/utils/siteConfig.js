c3d8c384138ce7d4bf3717f620102364bf876112
ysidhuitbit <ysidhu@itbit.com>
2019-12-19 11:30:36 -0500

    updated site URL in config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://gatsby.ghost.org`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://m3thods.netlify.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
